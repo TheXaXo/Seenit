e84087e2eff22526701f0054a9fc92df670b5fdb
TheXaXo <thexaxo90@gmail.com>
2018-04-22 10:59:19 +0300

    Ninja fixes.

--- src/main/java/com/thexaxo/seenit/SeenitApplication.java
+++ src/main/java/com/thexaxo/seenit/SeenitApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeenitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeenitApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SeenitApplication.class, args);
+    }
 }
